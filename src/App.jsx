eda43c95895df9ecfbf3ad5f89b8737b86eafc76
habeeb Oladapo <116345793+Hadeyemy@users.noreply.github.com>
2025-04-21 00:16:45 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [result, setResult] = useState(null)
 
   const handlePredict = async () => {
-    const response = await fetch('http://localhost:8000/predict', {
+    const response = await fetch('https://aviator-backend-v3b3.onrender.com', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
